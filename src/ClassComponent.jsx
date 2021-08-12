77f50e179d3c70af25584dab8836fb6732575d37
DIALLO <amadou9393@yahoo.fr>
2021-08-12 01:10:48 +0000

    install react-route-dom

--- src/ClassComponent.jsx
+++ src/ClassComponent.jsx
@@ -9,22 +9,32 @@ class ClassComponent extends Component {
 
     }
     componentDidMount() {
-        fetch("https://api.nytimes.com/svc/movies/v2/critics/all.json")
-        .then((res)=>res.json())
-        .then((movie)=>{
-            console.log("movie:",movie )
-            this.setState({movies: movie})
-        })
-        .catch((Error)=>console.log(Error));
+        fetch( "https://api.nytimes.com/svc/movies/v2/reviews/all.json?query=&api-key=EL60qpjMJbSG8Gi3c7ozM9h4GRpInBwR")
+            .then((res) => res.json())
+            .then((movie) => {
+
+                console.log("movie:", movie.results)
+                this.setState({ movies: movie.results })
+            })
+            .catch((Error) => console.log(Error));
     }
     render() {
         return (
             <div>
-             <h1>All movies</h1>
-             {this.state.movies.map((mov,id)=>{
-                 return
-                 <h3>{}</h3>
-             })}
+                <h1>All movies fetched by class component</h1>
+                {
+                    this.state.movies.map((mov, id) => {
+                        return (
+                            <div key={mov.id} style={{ border: '1px solid black',marginLeft:'20rem',marginRight:'20rem',marginTop:'1rem',paddingTop:'1rem' }}>
+                               <img src={mov.multimedia.src} alt="my_movies" /> 
+                                <div>{mov.display_title}</div>
+                                <div>{mov.critics_pick}</div>
+                                <div>{mov.byline}</div>
+                            </div>
+                        )
+
+                    })
+                }
             </div>
         );
     }
--- src/FunctionComponent.jsx
+++ src/FunctionComponent.jsx
@@ -2,24 +2,37 @@ import React, { useState , useEffect  } from 'react';
 import axios from 'axios';
 
 
-const FunctionComponent = () => {
-     const [movis, setMovis] = useState([]);
+function FunctionComponent () {
+    let movis=[];
+     const [movies, setMovies] = useState(movis);
      useEffect(()=>{
-         axios.get("https://api.nytimes.com/svc/movies/v2/critics/all.json")
-         .then((res)=>{
-             console.log(res)
-             setMovis(res.data)
-         }).catch((error)=>console.log("error"))
+         async function getmovies(){
+          let res = await fetch("https://api.nytimes.com/svc/movies/v2/reviews/all.json?query=&api-key=EL60qpjMJbSG8Gi3c7ozM9h4GRpInBwR")
+          let mov = await res.json()
+          console.log(mov.results)
+          setMovies(mov.results)
+         }
+         getmovies()
+        
 
-     })
+     },[])
     return (
         <div>
-           <h1>All movies in Funtions</h1>
-             {movis.map((mov,id)=>{
-                 return
-                 <h3>{}</h3>
-             })} 
-        </div>
+                <h1>All movies fetched by function component</h1>
+                {
+                movies.map((movi)=>{
+                        return (
+                            <div key={movi.id} style={{ border: '1px solid black',marginLeft:'20rem',marginRight:'20rem',marginTop:'1rem',paddingTop:'1rem' }}>
+                               <img src={movi.multimedia.src} /> 
+                                <div>{movi.display_title}</div>
+                                <div>{movi.critics_pick}</div>
+                                <div>{movi.byline}</div>
+                            </div>
+                        )
+
+                    })
+                }
+            </div>
     );
 }
 
